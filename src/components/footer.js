d55bfb088401ef6c729df3091215732751df6b21
PensadEnFlebas <goblininc20@gmail.com>
2024-10-26 19:26:55 +0200

    REFACTOR footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -17,6 +17,95 @@ function FOOTER() {
   pyramidImg.setAttribute('loading', 'lazy')
   pyramidDiv.append(pyramidImg)
 
+  let infoContainer = document.createElement('div')
+  infoContainer.classList.add('infoContainer')
+
+  let policiesDiv = document.createElement('div')
+  policiesDiv.classList.add('policiesDiv')
+
+  // let policiesTitle = document.createElement('h3')
+  // policiesTitle.textContent = 'Information'
+  // policiesDiv.append(policiesTitle)
+
+  let policiesList = document.createElement('ul')
+  let policies = ['Privacy Policy', 'Terms of Service', 'Who we are']
+
+  policies.forEach((policy) => {
+    let li = document.createElement('li')
+    let a = document.createElement('a')
+    a.setAttribute('href', '#')
+    a.textContent = policy
+    li.append(a)
+    policiesList.append(li)
+  })
+  policiesDiv.append(policiesList)
+  infoContainer.append(policiesDiv)
+
+  let copyrightDiv = document.createElement('div')
+  let copyrightLink = document.createElement('a')
+  let copyrightImg = document.createElement('img')
+  let copyrightP = document.createElement('p')
+  copyrightDiv.classList.add('copyrightDiv')
+  copyrightLink.classList.add('copyrightLink')
+  copyrightImg.classList.add('copyrightImg')
+  copyrightLink.setAttribute('href', '#')
+  copyrightImg.setAttribute(
+    'src',
+    'https://res.cloudinary.com/dd0zwrz0b/image/upload/v1728733783/Project%202%20%7C%20Filters%20shopping:%20Goblin%20Heavy%20Metal%20Store/Goblin%20Store%20Logos/Favicon_Goblin_Store_skp6wk.png'
+  )
+  copyrightImg.setAttribute('alt', 'Goblin Heavy Metal Store Logo')
+  copyrightImg.setAttribute('loading', 'lazy')
+  copyrightP.textContent =
+    '© 2024 Goblin Heavy Metal Store. All rights reserved.'
+
+  copyrightLink.append(copyrightImg)
+  copyrightDiv.append(copyrightLink, copyrightP)
+  infoContainer.append(copyrightDiv)
+
+  let socialDiv = document.createElement('div')
+  socialDiv.classList.add('socialDiv')
+
+  // let socialTitle = document.createElement('h3')
+  // socialTitle.textContent = 'Follow Us'
+  // socialDiv.append(socialTitle)
+
+  let socialIcons = [
+    'https://res.cloudinary.com/dd0zwrz0b/image/upload/v1729810104/Project%202%20%7C%20Filters%20shopping:%20Goblin%20Heavy%20Metal%20Store/Icons/facebook-logo-facebook-icon-transparent-free-png_ggm8hf.webp',
+    'https://res.cloudinary.com/dd0zwrz0b/image/upload/v1729809860/Project%202%20%7C%20Filters%20shopping:%20Goblin%20Heavy%20Metal%20Store/Icons/spotify-logo-spotify-social-media-icon-free-png_cyi4rb.webp',
+    'https://res.cloudinary.com/dd0zwrz0b/image/upload/v1729809894/Project%202%20%7C%20Filters%20shopping:%20Goblin%20Heavy%20Metal%20Store/Icons/img.icons8_eb4fk6.png',
+    'https://res.cloudinary.com/dd0zwrz0b/image/upload/v1729810137/Project%202%20%7C%20Filters%20shopping:%20Goblin%20Heavy%20Metal%20Store/Icons/instagram-logo-instagram-icon-transparent-free-png_pdplnz.webp',
+    'https://res.cloudinary.com/dd0zwrz0b/image/upload/v1729810183/Project%202%20%7C%20Filters%20shopping:%20Goblin%20Heavy%20Metal%20Store/Icons/twitter-new-logo-twitter-icons-new-twitter-logo-x-2023-x-social-media-icon-free-png_myhtpd.webp',
+    'https://res.cloudinary.com/dd0zwrz0b/image/upload/v1729810214/Project%202%20%7C%20Filters%20shopping:%20Goblin%20Heavy%20Metal%20Store/Icons/tiktok-logo-tiktok-logo-transparent-tiktok-icon-transparent-free-free-png_y0s4yb.webp'
+  ]
+
+  let socialLink = [
+    'Facebook icon',
+    'Spotify icon',
+    'YouTube icon',
+    'Instagram icon',
+    'Twitter icon',
+    'Tik Tok icon'
+  ]
+
+  let socialList = document.createElement('ul')
+
+  socialIcons.forEach((icon, i) => {
+    let li = document.createElement('li')
+    let a = document.createElement('a')
+    let img = document.createElement('img')
+    img.classList.add('footerIcon')
+    img.setAttribute('src', icon)
+    img.setAttribute('alt', socialLink[i])
+    img.setAttribute('loading', 'lazy')
+    a.setAttribute('href', '#')
+
+    a.append(img)
+    li.append(a)
+    socialList.append(li)
+  })
+  socialDiv.append(socialList)
+  infoContainer.append(socialDiv)
+
   let zombieDiv = document.createElement('div')
   zombieDiv.classList.add('zombieDiv')
   let zombieImg = document.createElement('img')
@@ -30,6 +119,7 @@ function FOOTER() {
   zombieDiv.append(zombieImg)
 
   const footer = document.querySelector('footer')
+  infoContainer.append(policiesDiv, copyrightDiv, socialDiv)
+  containerFooterDiv.append(pyramidDiv, infoContainer, zombieDiv)
   footer.append(containerFooterDiv)
-  containerFooterDiv.append(pyramidDiv, zombieDiv)
 }
--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@
 
 import { HEADER } from './components/header.js'
 HEADER()
-import { FOOTER } from './components/footer.js'
-FOOTER()
 import { mainTitle } from './components/body-main/mainTitle.js'
 mainTitle()
 import { searchMenu } from './components/body-main/searchMenu.js'
 searchMenu()
+import { FOOTER } from './components/footer.js'
+FOOTER()
