61f8ea8488bc3caa2d9c7fbbc37158311814fa3f
PensadEnFlebas <169026191+PensadEnFlebas@users.noreply.github.com>
2024-11-03 12:07:59 +0100

    Revert "Hover disabler merge into search-form"

--- src/components/body-main/searchMenu.js
+++ src/components/body-main/searchMenu.js
@@ -1,6 +1,3 @@
-import { searchForm } from './searchForm'
-import { hoverDisabler } from './hover-disabler'
-
 export { searchMenu }
 
 function searchMenu() {
@@ -37,35 +34,4 @@ function searchMenu() {
   searchMenuButton.append(p, searchMenuIcon)
   searchMenuContainer.append(searchMenuButton, resetMenuButton)
   main.append(searchMenuContainer)
-
-  hoverDisabler(searchMenuButton)
-  hoverDisabler(resetMenuButton)
-
-  searchMenuButton.addEventListener('click', function () {
-    searchMenuContainer.classList.toggle('expanded')
-    searchMenuButton.classList.toggle('expanded')
-    resetMenuButton.classList.toggle('contracted')
-
-    if (searchMenuContainer.classList.contains('expanded')) {
-      p.textContent = `EXIT`
-      searchMenuIcon.setAttribute(
-        'src',
-        './src/assets/The Shining Exit Animated.gif'
-      )
-      searchMenuIcon.setAttribute(
-        'alt',
-        'The Shining Movie pixel art exit icon'
-      )
-      searchForm()
-    } else {
-      p.textContent = `SEARCH`
-      searchMenuIcon.setAttribute('src', './src/assets/Search Menu Eye.gif')
-      searchMenuIcon.setAttribute('alt', 'Eye searching Icon')
-
-      const form = document.querySelector('form')
-      if (form) {
-        form.remove()
-      }
-    }
-  })
 }
